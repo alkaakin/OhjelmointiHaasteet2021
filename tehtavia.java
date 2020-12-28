097edc50cc7fc90fd85003d2b420c651495d3e1c
Aleksi Kaakinen <aleksi.kaakinen@outlook.com>
2020-12-28 16:02:34 +0200

    Uusimmat

---
+++ tehtavia.java
@@ -0,0 +1,36 @@
+
+    import java.util.*;
+public class tehtavia {
+   
+public static void main(String[] args) {
+    
+    //toistetaan stringiä neliön muodossa annetut rivit (SAIPPUAKAUPPIAS, 47x47)
+    //kaksi laskuria, i= sananpituus, i2 = viimeinen rivi
+    // i = 14 (SAIPPUAKAUPPIAS.length), i2 = 46 
+
+    
+
+
+int rivilaskuri = 0;
+int merkkilaskuri = 0;
+String [] Saippuakauppias = new String []{"S","A","I","P","P","U","A","K","A","U","P","P","I","A","S"};
+
+int i = 6;
+while (i < 10) {
+if (merkkilaskuri + 14 > 47) {
+int erotus = 47 - merkkilaskuri;
+
+//while
+}
+System.out.print("SAIPPUAKAUPPIAS");
+merkkilaskuri += 14;
+System.out.print(Saippuakauppias[i]);
+i++;
+}
+
+
+}
+
+
+
+}
