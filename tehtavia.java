ad1bb4788b232e8234f93c399597c587aee8765c
Aleksi Kaakinen <aleksi.kaakinen@outlook.com>
2020-12-29 09:36:43 +0200

    Yritys

--- tehtavia.java
+++ tehtavia.java
@@ -15,17 +15,28 @@ int rivilaskuri = 0;
 int merkkilaskuri = 0;
 String [] Saippuakauppias = new String []{"S","A","I","P","P","U","A","K","A","U","P","P","I","A","S"};
 
-int i = 6;
-while (i < 10) {
+while (true) {
 if (merkkilaskuri + 14 > 47) {
 int erotus = 47 - merkkilaskuri;
+int i = 0;
+    while(i<erotus) {
+        System.out.print(Saippuakauppias[i]);
+        i++;
+    }
 
-//while
+//Vaihda riviä
+merkkilaskuri = 1;
+System.out.println("");
+//Jatka printtaamista
+
+
+
+
 }
 System.out.print("SAIPPUAKAUPPIAS");
 merkkilaskuri += 14;
-System.out.print(Saippuakauppias[i]);
-i++;
+//System.out.print(Saippuakauppias[i]);
+//i++;
 }
 
 
