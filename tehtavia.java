89e211a43de7bb4bddc8569f683b61dfea985955
Aleksi Kaakinen <aleksi.kaakinen@outlook.com>
2020-12-29 20:28:36 +0200

    lopullinen versio

--- tehtavia.java
+++ tehtavia.java
@@ -1,47 +1,44 @@
 
     import java.util.*;
 public class tehtavia {
-   
-public static void main(String[] args) {
-    
-    //toistetaan stringiä neliön muodossa annetut rivit (SAIPPUAKAUPPIAS, 47x47)
-    //kaksi laskuria, i= sananpituus, i2 = viimeinen rivi
-    // i = 14 (SAIPPUAKAUPPIAS.length), i2 = 46 
 
-    
+public static void main(String[] args) {
 
 
 int rivilaskuri = 0;
 int merkkilaskuri = 0;
 String [] Saippuakauppias = new String []{"S","A","I","P","P","U","A","K","A","U","P","P","I","A","S"};
 
-while (true) {
-if (merkkilaskuri + 14 > 47) {
-int erotus = 47 - merkkilaskuri;
-int i = 0;
+
+while (rivilaskuri<47) {
+    int i = 0;
+    if (merkkilaskuri + 15 > 47) {
+    int erotus = 47 - merkkilaskuri;
+  
     while(i<erotus) {
         System.out.print(Saippuakauppias[i]);
         i++;
     }
 
-//Vaihda riviä
-merkkilaskuri = 1;
-System.out.println("");
-//Jatka printtaamista
+    System.out.println();
+    rivilaskuri++;
+    merkkilaskuri = 0;
 
+    while (i<Saippuakauppias.length) {
+        System.out.print(Saippuakauppias[i]);
+        merkkilaskuri++;
+        i++;
+    }
+    }
 
+else {
+    System.out.print("SAIPPUAKAUPPIAS");
+    merkkilaskuri += 15;
+    }
 
+    i = 0;
+}
 
 }
-System.out.print("SAIPPUAKAUPPIAS");
-merkkilaskuri += 14;
-//System.out.print(Saippuakauppias[i]);
-//i++;
-}
-
-
-}
-
-
 
 }
